f6c6d419239e1cf47810cb7f382dddda4ca65c0f
Timur <selleramz@gmail.com>
2019-01-29 22:02:34 +0100

    created classes and instanciated them

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const eventBrite = new Eventbrite ();
+const ui = new UI ();
+
+console.log(eventBrite);
---
+++ js/eventbrite.js
@@ -0,0 +1,5 @@
+class Eventbrite {
+    constructor (){
+        this.auth_token = '5FFTBLR2QX7BG6MPRMM6';
+    }
+}
