3d93bca36a4af0cbefbf3cb2d3a275e02db29384
Timur <selleramz@gmail.com>
2019-01-29 22:52:12 +0100

    Created listener for submitting and added a validation of input for the event

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 const eventBrite = new Eventbrite ();
 const ui = new UI ();
+
+//Listener for the submit button
+document.getElementById('submitBtn').addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    const eventName = document.getElementById('event-name').value;
+    const category = document.getElementById('category').value;
+
+    // Validation of the input
+    if (eventName !== "") {
+
+    } else {
+        ui.printMessage('Add an Event or City', 'text-center alert alert-danger mt-4');
+    }
+
+});
--- js/ui.js
+++ js/ui.js
@@ -24,4 +24,25 @@ class UI {
                 })
                 .catch (error =>{window.alert('There is an error in the server connection')})
     }
+
+    printMessage(message, cssclass){
+        const div = document.createElement('div');
+        div.className = cssclass;
+        div.appendChild(document.createTextNode(message));
+
+        const searchDiv = document.querySelector('#search-events');
+        searchDiv.appendChild(div);
+
+        // remove the alert after 3 seconds
+        setTimeout(()=>{
+            this.removeMessage();
+        }, 3000);
+   }
+
+   removeMessage(){
+       const alert = document.querySelector('.alert');
+       if (alert){
+        alert.remove();
+   }
+}
 }
